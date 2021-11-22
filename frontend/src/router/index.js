18853819bd63ba24ec7789a9217e2d4ebc47c4a1
19373225tangyu <1963086410@qq.com>
2021-11-22 17:37:58 +0800

    login0

---
+++ frontend/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import LogReg from '../../views/login_register'
+import newLog from '../../views/newLogin'
+import nop from '../../views/nop'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
+    // {
+    //   path: '/',
+    //   name: 'LogReg',
+    //   component: LogReg
+    // },
+    {
+      path: '/',
+      name: 'newLog',
+      component: newLog
+    },
+    {
+      path: '/nop',
+      name:'nop',
+      component: nop
+    }
+  ]
+})
